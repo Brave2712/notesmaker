27d9b33ae146cb513ddfc1a1cf111cebbd760ff0
Brave2712 <braviarygamer2712@gmail.com>
2026-06-24 14:49:43 +0530

    Update theme.js

--- NotesMaker/static/theme.js
+++ NotesMaker/static/theme.js
@@ -1,7 +1,33 @@
 const toggleBtn = document.getElementById("themeToggle");
 
+function applyTheme(theme){
+
+    if(theme === "light"){
+        document.body.classList.add("light-mode");
+    }
+    else{
+        document.body.classList.remove("light-mode");
+    }
+
+}
+
+const savedTheme = localStorage.getItem("theme") || "dark";
+
+applyTheme(savedTheme);
+
 if(toggleBtn){
+
     toggleBtn.addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
+
+        const newTheme =
+            document.body.classList.contains("light-mode")
+            ? "dark"
+            : "light";
+
+        localStorage.setItem("theme", newTheme);
+
+        applyTheme(newTheme);
+
     });
+
 }
